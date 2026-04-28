3310c5109cbc3c14f562c3da995ea02ba5e2bd04
LorianaD <loriana.diano@gmail.com>
2026-04-28 16:55:02 +0200

    add : player

--- src/Controller/PlayerController.php
+++ src/Controller/PlayerController.php
@@ -2,19 +2,47 @@
 
 namespace App\Controller;
 
+use App\Entity\Player;
+use App\Form\PlayerType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-#[Route('/dashboard')]
+#[Route('/dashboard/player')]
 final class PlayerController extends AbstractController
 {
-    #[Route('/player', name: 'player_index')]
+    #[Route('/', name: 'player_index')]
     public function index(): Response
     {
 
         return $this->render('player/index.html.twig', [
             'controller_name' => 'PlayerController',
         ]);
+    }
+
+    #[Route('/new', name: 'player_new', methods:['GET' , 'POST'])]
+    public function new(Request $request, EntityManagerInterface $em)
+    {
+
+        $newPlayer = new Player;
+
+        $formPlayer = $this->createForm(PlayerType::class , $newPlayer);
+        $formPlayer->handleRequest($request);
+
+        if ($formPlayer->isSubmitted() && $formPlayer->isValid()) {
+
+            $em ->persist($newPlayer);
+            $em->flush();
+
+            return $this->redirectToRoute('player_index');
+
+        }
+
+        return $this->render('player/new.html.twig',[
+            'newPlayerForm' => $formPlayer,
+        ]);
+
     }
 }
---
+++ src/Form/PlayerType.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Player;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class PlayerType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('lastname')
+            ->add('firstname')
+            ->add('birth_date', null, [
+                'widget' => 'single_text',
+            ])
+            ->add('number')
+            ->add('address')
+            ->add('pc')
+            ->add('city')
+            ->add('phone_number')
+            ->add('email')
+            ->add('license_number')
+            // ->add('Submit', SubmitType::class, [
+            //     'label'    => 'Envoyer'
+            // ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Player::class,
+        ]);
+    }
+}
